aa286eeaa1c959735c56c8a16c464148df368284
Nicky Liu <nicky@zedotech.com>
2024-05-01 19:35:09 +0800

    加入專案檔案。

---
+++ WebApplicationFrontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationFrontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
